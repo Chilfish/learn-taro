d1551c64306ab4757f549e4fc1eccb9af3e12e1b
Chill Fish <chill4fish@gmail.com>
2023-07-20 13:09:38 +0800

    feat(store): using pinia

--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { useCounterStore } from '@/store/counter'
 
+const counterStore = useCounterStore()
 </script>
 
 <template>
-  <view class="me">
-    <text>Me</text>
-  </view>
+  <div>
+    <h1>Profile</h1>
+    <p>Count: {{ counterStore.count }}</p>
+    <button @click="counterStore.increment">
+      Increment
+    </button>
+  </div>
 </template>
