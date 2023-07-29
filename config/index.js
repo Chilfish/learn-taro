d14ec21c0143bbd2f27d62ec416576140ea244fb
Chill Fish <chill4fish@gmail.com>
2023-07-29 16:21:35 +0800

    fix(config): use taro-plugin-vant-weapp

--- config/index.js
+++ config/index.js
@@ -21,7 +21,10 @@ const config = {
     '@': path.resolve(root, 'src'),
     '@cp': path.resolve(root, 'src/components'),
   },
-  plugins: ['@tarojs/plugin-html'],
+  plugins: [
+    '@tarojs/plugin-html',
+    'taro-plugin-vant-weapp',
+  ],
   defineConstants: {},
   copy: {
     patterns: [
@@ -38,9 +41,9 @@ const config = {
   },
   framework: 'vue3',
   compiler: 'webpack5',
-  // cache: {
-  //   enable: true, // 不要开启缓存，还没解决配置文件的缓存一致性问题，导致修改配置文件后，不生效
-  // },
+  cache: {
+    enable: false, // 不要开启缓存，还没解决配置文件的缓存一致性问题，导致修改配置文件后，不生效
+  },
   mini: {
     hot: true,
     postcss: {
